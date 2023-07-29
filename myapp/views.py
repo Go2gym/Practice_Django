da765dcc10ed08bed3c8aeafedb2c37792e41b57
Boiling SODA <go2gym365@gmail.com>
2023-07-29 10:56:32 +0000

    ..

---
+++ myapp/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, HttpResponse
+import random
+
+topics = [
+    {'id':1, 'title':'Routing', 'body':'Routing is..'},
+    {'id':2, 'title':'View', 'body':'View is..'},
+    {'id':3, 'title':'Model', 'body':'Model is..'},
+]
+
+def HTMLTemplate(articleTag):
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return f'''
+    <html>
+    <body>
+        <h1><a href="/">Django</h1>
+        <ul>
+            {ol}
+        </ul>        
+        {articleTag}
+        <ul>
+            <li><a href="/create/">create</a></li>
+        </ul> 
+    </body>
+    </html>
+    '''
+
+def index(request):
+    article = '''
+    <h2>Welcome</h2>
+        Hello, Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
+
+def read(request, id):
+    global topics
+    article = ''
+    for topic in topics:
+        if topic['id'] == int(id):
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article))
+
+def create(request):
+    article = '''
+        <form action="/create/">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p><textarea name="body" placeholder="body"></textarea></p>
+            <p><input type="submit"></p>
+        </form>
+    '''
+    return HttpResponse(HTMLTemplate(article))
