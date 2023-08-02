f2054aff8cf3c2322a1c5ac2941e1873b1e0bf80
Boiling SODA <go2gym365@gmail.com>
2023-08-02 13:59:48 +0000

    ..

--- myBlog/main/urls.py
+++ myBlog/main/urls.py
@@ -4,9 +4,8 @@ from django.contrib import admin
 from main.views import index, blog, posting
 
 urlpatterns = [
-    path('', index, name='index'),
     path('admin/', admin.site.urls),
-    path('blog/', blog, name='blog'),
+    path('', blog, name='blog'),
     # blog뒤에 숫자가 온다면 그 값을 pk라고 할것이다. pk값도 같이 전달
-    path('blog/<int:pk>', posting, name='posting'),
+    path('<int:pk>/', posting, name="posting"),
 ]
