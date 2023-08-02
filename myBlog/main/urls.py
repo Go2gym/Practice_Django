850b7477486bfeae96da4cd3832c8db9eba2e5f5
Boiling SODA <go2gym365@gmail.com>
2023-08-02 11:27:39 +0000

    ...

--- myBlog/main/urls.py
+++ myBlog/main/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from . import views
-
+from django.contrib import admin
+from main.views import index, blog, posting
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
+    path('admin/', admin.site.urls),
+    path('blog/', blog, name='blog'),
+    # blog뒤에 숫자가 온다면 그 값을 pk라고 할것이다. pk값도 같이 전달
+    path('blog/<int:pk>', posting, name='posting'),
 ]
--- myBlog/main/views.py
+++ myBlog/main/views.py
@@ -6,8 +6,14 @@ def index(request):
     return render(request,'main/index.html')
 
 # blog.html 페이지를 부르는 blog함수
-def bolg(request):
+def blog(request):
     # 모든 Post를 가져와 postlist에 저장
     postlist = Post.objects.all()
-    #bolg.html페이지를 열때 모든 Post인 postlist도 같이 가져옴
-    return render(request, 'main/bolg.html', {'postlist':postlist})
+    #blog.html페이지를 열때 모든 Post인 postlist도 같이 가져옴
+    return render(request, 'main/blog.html', {'postlist':postlist})
+
+def posting(request, pk):
+    #게시글 중 primary_key를 이용해 하나의 게시글을 검색
+    post = Post.objects.get(pk=pk)
+    # posting.html 페이지를 열 때 찾아낸 게시글을 post라는 이름으로 가져옴
+    return render(request, 'main/posting.html', {'post': post})
